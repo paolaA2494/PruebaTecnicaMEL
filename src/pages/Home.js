5c6efc75b24f97e98f850565d967a7bc14f50904
Casta <palolahrn0104@gmail.com>
2021-08-09 00:22:37 -0500

    Create project with react, add structure project with the require files create search component and the first view

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import '../styles/Home.scss';
+import Search from '../components/Search';
+
+
+
+function Home() {
+  return (
+    <div className="home">
+      <Search />
+    </div>
+  );
+}
+
+export default Home;
