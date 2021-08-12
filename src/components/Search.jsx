7ce6a2179ead0916898bd263c3419ece37d9444e
Paola Andrea Hernández <palolahrn0104@gmail.com>
2021-08-12 17:24:09 -0500

    change name styles for a many components

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,34 +1,34 @@
-import React, { Component } from 'react';
-import '../styles/Search.scss';
-import logo from '../assets/images/Logo_ML.png';
-import iconSearch from '../assets/images/ic_SearchG.png';
-
+import React, { Component } from "react";
+import "../styles/Search.scss";
+import logo from "../assets/images/Logo_ML.png";
+import iconSearch from "../assets/images/ic_SearchG.png";
 
 class Search extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {}
-    }
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
-
-
-    render() {
-        return (
-            <div className="search">
-                <div className="search__content">
-                    <div className="search__content--logo">
-                        <img src={logo} alt="logo" />
-                    </div>
-                    <div className="search__content--searhbar">
-                        <input className="search__content--searhbar-input" placeholder="Nunca dejes de buscar" />
-                        <button  className="search__content--searhbar-icon">
-                        <img src={iconSearch} alt="icon" />
-                        </button>
-                    </div>
-                </div>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="search">
+        <div className="search-content">
+          <div className="search-content__logo">
+            <img src={logo} alt="logo" />
+          </div>
+          <div className="search-content__search-bar">
+            <input
+              className="search-content__search-bar--input"
+              placeholder="Nunca dejes de buscar"
+            />
+            <button className="search-content__search-bar--icon">
+              <img src={iconSearch} alt="icon" />
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Search;
